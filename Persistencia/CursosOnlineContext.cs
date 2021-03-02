9062c69ad008de4b1e348f34cf7cc975706de7c1
Eduardo Aguilar Olivares <eaomxlheao@gmail.com>
2021-03-01 22:17:54 -0600

    Setup de context y caneda de conexion

---
+++ Persistencia/CursosOnlineContext.cs
@@ -0,0 +1,29 @@
+﻿using Dominio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Persistencia
+{
+    public class CursosOnlineContext : DbContext
+    {
+        private const string connectionString = @"Data Source=localhost\sqlexpress; Initial Catalog=CursosOnline; Integrated Security=true;";
+
+        public CursosOnlineContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CursoInstructor>().HasKey(ci => new { ci.CursoId, ci.InstructorId });
+        }
+
+        public DbSet<Curso> Curso { get; set; }
+        public DbSet<Precio> Precio { get; set; }
+        public DbSet<Comentario> Comentario { get; set; }
+        public DbSet<Instructor> Instructor { get; set; }
+        public DbSet<CursoInstructor> CursoInstructor { get; set; }
+    }
+}
